455ff792c042487af5507668e57f8a668099666d
unknown <thomas.menard@ecam-rennes.com>
2019-12-09 22:16:11 +0100

    tableau avec les données + incorporation de bootstrap

--- js/ISS.js
+++ js/ISS.js
@@ -6,36 +6,36 @@ var positioncallBackGetSuccess = function(data) {
   element1.innerHTML = data.iss_position.latitude + "°";
   element2.innerHTML = data.iss_position.longitude + "°";
 
-
-
   var lat = parseFloat(data.iss_position.latitude);
   var lng = parseFloat(data.iss_position.longitude);
 
-  console.log(lat)
-  console.log(lng)
+  console.log(lat);
+  console.log(lng);
 
-  var pos = {lat: lat, lng: lng};
-  
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: pos});
+  var pos = { lat: lat, lng: lng };
 
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: pos
+  });
 
-  var marker = new google.maps.Marker({position: pos, map: map, animation: google.maps.Animation.DROP });
-
-
-
+  var marker = new google.maps.Marker({
+    position: pos,
+    map: map,
+    animation: google.maps.Animation.DROP
+  });
 };
 
 var peoplecallBackGetSuccess = function(data) {
   console.log(data);
   var element = document.getElementById("ISS_people");
   var nb = data.number;
-  element.innerHTML = "Il y a actuellement : " + nb + " personnes dans l'ISS";
+  element.innerHTML = nb + " personnes dans l'ISS";
 
-  //   for (var i = 0; i < nb ; i++) {
-  // var element = document.getElementById("people_" + i);
-  // element.innerHTML = data.people.i;
-  // console.log(data.people.i);
-  //   }
+  for (var i = 0; i < nb ; i++) {
+  var element = document.getElementById("people_" + i);
+  element.innerHTML = data.people[i].name;
+  
 };
 
 function position() {
@@ -44,5 +44,3 @@ function position() {
   var url2 = "http://api.open-notify.org/astros.json";
   $.get(url2, peoplecallBackGetSuccess).done(function() {});
 }
-
-
